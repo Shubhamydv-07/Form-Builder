7cb886c5cf60d9abc61845022e083a5e33767fbe
Shubham Yadav <144944097+Shubhamydv-07@users.noreply.github.com>
2025-08-16 00:47:54 +0530

    Update server.js
    cors update

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const path = require('path');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json({limit:"10mb"}));
 
 // Static uploads
